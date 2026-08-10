ddd974d0b418cad3423ae0842a614872984d3f2e
Okuyama Takaki <fgxwaop@gmail.com>
2026-08-10 22:41:49 +0900

    Initial commit

---
+++ src/main/java/com/example/myapp/MyappApplication.java
@@ -0,0 +1,20 @@
+package com.example.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class MyappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyappApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
